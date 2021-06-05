efca50eb1b636b231af251d0e032b5c3c0e51ee5
Fefutema <felipe.fut09@hotmail.com>
2021-06-05 10:38:21 -0300

    removendo solução

--- src/main/java/br/com/impacta/lab/ContaCorrente.java
+++
@@ -1,9 +0,0 @@
-package br.com.impacta.lab;
-
-public class ContaCorrente extends Conta {
-
-	public ContaCorrente(int numero, int agencia, String tipo) {
-		super(numero, agencia, tipo);
-	}
-
-}
--- src/main/java/br/com/impacta/lab/ContaPoupanca.java
+++
@@ -1,9 +0,0 @@
-package br.com.impacta.lab;
-
-public class ContaPoupanca extends Conta {
-
-	public ContaPoupanca(int numero, int agencia, String tipo) {
-		super(numero, agencia, tipo);
-	}
-
-}
